2dc3363520a6b56f8036e29cc8d59b3d881f04c8
Liliya <vapioo12345>
2023-03-27 12:30:44 +0200

    added  default basic SecurityConfig

---
+++ src/main/java/com/liliya/shop/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.liliya.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain security(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                //права доступа
+                .antMatchers("/api/order/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin().disable()
+                .httpBasic().disable()
+                .cors().disable()
+                .csrf().disable();
+        return http.build();
+
+    }
+}
