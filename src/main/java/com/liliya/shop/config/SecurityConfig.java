1fc6ea149cc5f6d31c1df9e823bdcd8de7082236
Liliya <vapioo12345>
2023-03-28 10:14:01 +0200

    Added  2 hardcoded users

--- src/main/java/com/liliya/shop/config/SecurityConfig.java
+++ src/main/java/com/liliya/shop/config/SecurityConfig.java
@@ -4,25 +4,58 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain security(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                //права доступа
-                .antMatchers("/api/orders/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
-                .antMatchers("/api/**").permitAll()
-               .anyRequest().permitAll()
-                .and().formLogin().disable()
+    public SecurityFilterChain security(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests(this::configureAuthorization)
+
+                .formLogin().disable()
                 .httpBasic().disable()
                 .cors().disable()
                 .csrf().disable();
         return http.build();
     }
 
+    private void configureAuthorization(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config) {
+        config
+                //права доступа
+                .antMatchers("/api/order/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
