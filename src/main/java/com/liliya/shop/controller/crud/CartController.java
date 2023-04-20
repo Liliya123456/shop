e7a21ce317c149c2af692531b21e55b2d0253f25
Liliya <vapioo12345>
2023-04-20 23:09:15 +0200

    fixed CartService

--- src/main/java/com/liliya/shop/controller/crud/CartController.java
+++ src/main/java/com/liliya/shop/controller/crud/CartController.java
@@ -1,11 +1,10 @@
 package com.liliya.shop.controller.crud;
 
-import com.liliya.shop.entity.Item;
 import com.liliya.shop.entity.Order;
-import com.liliya.shop.entity.User;
 import com.liliya.shop.repository.ItemRepository;
 import com.liliya.shop.repository.OrderRepository;
 import com.liliya.shop.repository.UserRepository;
+import com.liliya.shop.service.CartService;
 import com.liliya.shop.service.OrderService;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +13,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/cart", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CartController {
-    @Autowired
-    private EntityManager em;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -32,88 +26,35 @@ public class CartController {
     private ItemRepository itemRepository;
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private CartService cartService;
 
 
-    @GetMapping(path = {"", "/"})
+    @GetMapping(path = {""})
     public List<Order> listOrders(@Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
-        return orderRepository.findByUserId(user.getUsername());
+        return cartService.listOrders(user);
+
     }
 
     @RequestMapping(path = "/new", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     public Order createOrder(@RequestBody Order order, @Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
-        String email = user.getUsername();
-        User dbUser = userRepository.findById(email).get();
-        order.setId(null);
-        order.setUser(dbUser);
-        isItemExist(order);
-        return orderRepository.save(order);
+        return cartService.createOrder(order, user);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Order findOrderById(@Parameter(hidden = true) @AuthenticationPrincipal UserDetails user, @PathVariable(required = true) Long id) {
-        String email = user.getUsername();
-        Optional<Order> order = orderRepository.findByUserIdAndId(email, id);
-        if (order.isPresent()) {
-            return order.get();
-        } else
-            throw new IllegalArgumentException("Did not find order");
+        return cartService.findOrderById(user, id);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     public Order update(@RequestBody Order order, @PathVariable(required = true) Long id, @Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
-        String email = user.getUsername();
-        Optional<Order> dbOrder = orderRepository.findByUserIdAndId(email, id);
-        if (dbOrder.isPresent()) {
-            order.setId(id);
-            order.setUser(dbOrder.get().getUser());
-
-            //TODO выделить проверку по товарам в отдельнеый метод  и добавть в создание.
-//            if (order.getItems() == null) {
-//                order.setItems(new ArrayList<>(0));
-//            } else {
-//                List<Item> items = itemRepository.findAllById(getAllid(order.getItems()));
-//                if (order.getItems().size() != items.size()) {
-//                    throw new IllegalArgumentException("All items must exist");
-//                } else {
-//                    order.setItems(items);
-//                }
-//            }
-            isItemExist(order);
-            return orderRepository.save(order);
-        } else
-            throw new IllegalArgumentException("Did not find  order");
+        return cartService.update(order, id, user);
     }
 
-    private List<Long> getAllid(List<Item> items) {
-        List<Long> result = new ArrayList<>(items.size());
-        for (Item item : items) {
-            result.add(item.getId());
-        }
-        return result;
-    }
-
-    private Order isItemExist(Order order) {
-        if (order.getItems() == null) {
-            order.setItems(new ArrayList<>(0));
-        } else {
-            List<Item> items = itemRepository.findAllById(getAllid(order.getItems()));
-            if (order.getItems().size() != items.size()) {
-                throw new IllegalArgumentException("All items must exist");
-            } else {
-                order.setItems(items);
-            }
-        }
-        return order;
-    }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void deleteOrder(@PathVariable(required = true) Long id, @Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
-        String email = user.getUsername();
-        Optional<Order> order = orderRepository.findByUserIdAndId(email, id);
-        if (order.isPresent()) {
-            orderRepository.deleteById(id);
-        } else
-            throw new IllegalArgumentException("Did not find order");
+        cartService.deleteOrder(id, user);
     }
 }
 
--- src/main/java/com/liliya/shop/service/CartService.java
+++ src/main/java/com/liliya/shop/service/CartService.java
@@ -1,15 +1,27 @@
 package com.liliya.shop.service;
 
+import com.liliya.shop.entity.Item;
+import com.liliya.shop.entity.Order;
+import com.liliya.shop.entity.User;
 import com.liliya.shop.repository.ItemRepository;
 import com.liliya.shop.repository.OrderRepository;
 import com.liliya.shop.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CartService {
-    @Autowired
-    private EntityManager em;
+
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -18,4 +30,81 @@ public class CartService {
     private ItemRepository itemRepository;
     @Autowired
     private OrderService orderService;
+
+
+    public List<Order> listOrders(UserDetails user) {
+        return orderRepository.findByUserId(user.getUsername());
+    }
+    public Order createOrder( Order order, UserDetails user) {
+        String email = user.getUsername();
+        User dbUser = userRepository.findById(email).get();
+        order.setId(null);
+        order.setUser(dbUser);
+        return orderRepository.save(isItemExist(order));
+    }
+    public Order findOrderById(UserDetails user, Long id) {
+        String email = user.getUsername();
+        Optional<Order> order = orderRepository.findByUserIdAndId(email, id);
+        if (order.isPresent()) {
+            return order.get();
+        } else
+            throw new IllegalArgumentException("Did not find order");
+    }
+    public Order update(Order order, Long id,  UserDetails user) {
+        String email = user.getUsername();
+        Optional<Order> dbOrder = orderRepository.findByUserIdAndId(email, id);
+        if (dbOrder.isPresent()) {
+            order.setId(id);
+            order.setUser(dbOrder.get().getUser());
+
+            //TODO выделить проверку по товарам в отдельнеый метод  и добавть в создание.
+//            if (order.getItems() == null) {
+//                order.setItems(new ArrayList<>(0));
+//            } else {
+//                List<Item> items = itemRepository.findAllById(getAllid(order.getItems()));
+//                if (order.getItems().size() != items.size()) {
+//                    throw new IllegalArgumentException("All items must exist");
+//                } else {
+//                    order.setItems(items);
+//                }
+//            }
+            return orderRepository.save(isItemExist(order));
+        } else
+            throw new IllegalArgumentException("Did not find  order");
+    }
+
+    public void deleteOrder(Long id, UserDetails user) {
+        String email = user.getUsername();
+        Optional<Order> order = orderRepository.findByUserIdAndId(email, id);
+        if (order.isPresent()) {
+            orderRepository.deleteById(id);
+        } else
+            throw new IllegalArgumentException("Did not find order");
+    }
+
+
+    private List<Long> getAllid(List<Item> items) {
+        List<Long> result = new ArrayList<>(items.size());
+        for (Item item : items) {
+            result.add(item.getId());
+        }
+        return result;
+    }
+
+    private Order isItemExist(Order order) {
+        if (order.getItems() == null) {
+            order.setItems(new ArrayList<>(0));
+        } else {
+            List<Item> items = itemRepository.findAllById(getAllid(order.getItems()));
+            if (order.getItems().size() != items.size()) {
+                throw new IllegalArgumentException("All items must exist");
+            } else {
+                order.setItems(items);
+            }
+        }
+        return order;
+    }
+
+
+
 }
